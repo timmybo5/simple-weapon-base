711fe3c48abb25960375f52633bb87d9dac57aec
Timm Luyten <timm.luyten@hotmail.com>
2022-12-11 21:38:59 +0200

    Add kill and noclip example commands

---
+++ code/ExampleCommands.cs
@@ -0,0 +1,43 @@
+﻿using Sandbox;
+
+namespace SWB_Base;
+
+internal class ExampleCommands
+{
+    [ConCmd.Server("kill")]
+    public static void KillCommand()
+    {
+        var client = ConsoleSystem.Caller;
+
+        if (client != null && client.Pawn is PlayerBase ply && ply.ActiveChild is WeaponBase weapon)
+        {
+            var damageInfo = new DamageInfo
+            {
+                Damage = ply.Health,
+                Weapon = weapon
+            };
+
+            ply.TakeDamage(damageInfo);
+        }
+    }
+
+    [ConCmd.Server("noclip")]
+    public static void NoclipCommand()
+    {
+        var client = ConsoleSystem.Caller;
+
+        if (client != null && client.Pawn is PlayerBase ply)
+        {
+            if (ply.DevController is PlayerNoclipController)
+            {
+                Log.Info("Noclip Mode Off");
+                ply.DevController = null;
+            }
+            else
+            {
+                Log.Info("Noclip Mode On");
+                ply.DevController = new PlayerNoclipController();
+            }
+        }
+    }
+}
