0e7f93ccf3d6c269124ac42a877137ec54c8e80b
Zachary <remscar@gmail.com>
2023-03-22 12:52:33 -0700

    Moved equip attachment command

--- code/ExampleCommands.cs
+++ code/ExampleCommands.cs
@@ -54,28 +54,4 @@ internal class ExampleCommands
             ply.TakeDamage(damageInfo);
         }
     }
-
-    [ConCmd.Server("swb_attachment_equip", Help = "Equips an attachment by name")]
-    public static void EquipAttachmentCMD(string name, bool enabled)
-    {
-        var client = ConsoleSystem.Caller;
-
-        if (client != null)
-        {
-            var player = client.Pawn as PlayerBase;
-
-            if (player.ActiveChild is not WeaponBase activeWeapon) return;
-
-            if (enabled && activeWeapon.GetAttachment(name) != null)
-            {
-                // Attach
-                _ = activeWeapon.EquipAttachmentSV(name);
-            }
-            else if (!enabled && activeWeapon.GetActiveAttachment(name) != null)
-            {
-                // Detach
-                activeWeapon.UnequipAttachmentSV(name);
-            }
-        }
-    }
 }
---
+++ code/swb_base/Commands.cs
@@ -0,0 +1,30 @@
+﻿using Sandbox;
+using SWB_Base;
+
+internal class Commands
+{
+    [ConCmd.Server("swb_attachment_equip", Help = "Equips an attachment by name")]
+    public static void EquipAttachmentCMD(string name, bool enabled)
+    {
+        var client = ConsoleSystem.Caller;
+
+        if (client != null)
+        {
+            var player = client.Pawn as PlayerBase;
+
+            if (player.ActiveChild is not WeaponBase activeWeapon)
+                return;
+
+            if (enabled && activeWeapon.GetAttachment(name) != null)
+            {
+                // Attach
+                _ = activeWeapon.EquipAttachmentSV(name);
+            }
+            else if (!enabled && activeWeapon.GetActiveAttachment(name) != null)
+            {
+                // Detach
+                activeWeapon.UnequipAttachmentSV(name);
+            }
+        }
+    }
+}
