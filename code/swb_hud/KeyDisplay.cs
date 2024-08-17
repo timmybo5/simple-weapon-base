9762a6439643ae4f118759da66c6653d49801129
sebbejohansson <sebbe.johansson1995@gmail.com>
2024-08-17 23:36:07 +0200

    Hide customization key when customizations are disabled

--- code/swb_hud/KeyDisplay.cs
+++ code/swb_hud/KeyDisplay.cs
@@ -36,7 +36,7 @@ public class KeyDisplay : Panel
 		SetClass( "hide", !isValidWeapon );
 		if ( !isValidWeapon ) return;
 
-		var showKey = activeWeapon.Attachments.Count == 0 || activeWeapon.IsCustomizing;
-		SetClass( "hide", showKey );
+		var hideKey = activeWeapon.Attachments.Count == 0 || activeWeapon.IsCustomizing || WeaponSettings.Instance.Customization == false;
+		SetClass( "hide", hideKey );
 	}
 }
