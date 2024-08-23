f24847c63edc7f96ce7a907376eaf71b5768e632
sebbejohansson <sebbe.johansson1995@gmail.com>
2024-08-23 16:20:05 +0200

    Simplified check on customization setting for key display.

--- code/swb_hud/KeyDisplay.cs
+++ code/swb_hud/KeyDisplay.cs
@@ -36,7 +36,7 @@ public class KeyDisplay : Panel
 		SetClass( "hide", !isValidWeapon );
 		if ( !isValidWeapon ) return;
 
-		var hideKey = activeWeapon.Attachments.Count == 0 || activeWeapon.IsCustomizing || WeaponSettings.Instance.Customization == false;
+		var hideKey = activeWeapon.Attachments.Count == 0 || activeWeapon.IsCustomizing || !WeaponSettings.Instance.Customization;
 		SetClass( "hide", hideKey );
 	}
 }
